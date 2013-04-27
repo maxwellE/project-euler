05ee3d943277bac3889150a58f94366b159635ac
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-04-27 11:14:04 -0400

    fixes for prime

--- largestprimefactor.lisp
+++ largestprimefactor.lisp
@@ -7,8 +7,10 @@
   (T (loop for i from 2 to (ceiling (/ (isqrt n) 2)) never
   (zerop (mod n i))))))
 
-(defun largest-value(seq)
-(loop for i in seq maximize i))
-
 (defun generate-primes(n)
 (loop for i from 1 upto (isqrt n) when (and (zerop (mod n i)) (is-prime i)) collect i))
+
+(defun find-biggest-prime(n)
+(loop for x in (generate-primes n) maximize x))
+
+(find-biggest-prime 600851475143)  
